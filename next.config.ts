9741786b59332fe1a94ce364b56851fea7af6b25
hitoon <19642497+hitoon@users.noreply.github.com>
2025-04-29 14:53:13 +0900

    deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // 静的ファイルのエクスポートを有効にする
+  reactStrictMode: true,
+  basePath: '/life-countdown-app',
 };
 
 export default nextConfig;
